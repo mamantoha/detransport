ab2ee457f133b6268485c1f907a970f005be897d
Anton Maminov <anton.linux@gmail.com>
2017-03-22 01:27:01 +0200

    add detransport api endpoints

--- app.rb
+++ app.rb
@@ -1,5 +1,36 @@
 require 'sinatra'
+require 'sinatra/json'
+require 'sinatra/reloader'
+require 'sinatra/namespace'
+require 'faraday'
+require 'json'
 
 class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+    register Sinatra::Namespace
+  end
+
+  namespace '/api' do
+    before do
+      url = 'http://api.detransport.com.ua'
+      @conn = Faraday.new(url: url)
+    end
+
+    get '/stops' do
+      response = @conn.get('/stops/list/')
+
+      data = JSON.parse(response.body)
+      json data
+    end
+
+    get '/vehicle/:id' do
+      response = @conn.post('/vehicles/info/', { stop: params[:id] })
+
+      data = JSON.parse(response.body)
+      json data
+    end
+
+  end
 
 end
