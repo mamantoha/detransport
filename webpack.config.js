df3536c8d0c57e8ced5393443b03d17ffcdbe40e
Anton Maminov <anton.linux@gmail.com>
2017-05-09 16:55:34 +0300

    try to fix deploy

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,14 @@ module.exports = {
     {
       test: /.vue$/,
       loader: 'vue-loader',
-      exclude: /node_modules/,
+      options: {
+        loaders: {
+          js: 'babel-loader',
+          file: 'file-loader',
+          scss: 'vue-style-loader!css-loader!postcss-loader!sass-loader',
+          sass: 'vue-style-loader!css-loader!postcss-loader!sass-loader?indentedSyntax'
+        }
+      },
     },
     { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports-loader?jQuery=jquery' },
     { test: /\.(woff2?|svg)$/, loader: 'url-loader?limit=10000' },
